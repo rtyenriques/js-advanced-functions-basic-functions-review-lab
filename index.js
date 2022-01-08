96ff7aa23ce766053342f8256b13fe2d52a3ce5c
= <rtyenriques@gmail.com>
2022-01-08 01:54:05 -0800

    commit tree 8f5e7c06a44b8c0727f10e1c4e03099da010e884

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function saturdayFun(arg = "roller-skate") {
+    return `This Saturday, I want to ${arg}!`
+}
+
+const mondayWork = function(arg = "go to the office") {
+    return `This Monday, I will ${arg}.`
+}
+
+function wrapAdjective(argu = '*') {
+    return function(arg = "special") {
+        return `You are ${argu}${arg}${argu}!`
+    }
+}
+
+const Calculator = {
+    add: function() {
+        return 1 + 3
+    },
+    subtract: function() {
+        return 1 - 3
+    },
+    multiply: function() {
+        return 1 * 3
+    },
+    divide: function() {
+        return 10 / 5
+    }
+
+}
+
+function actionApplyer(int, array) {
+    return int
+}
